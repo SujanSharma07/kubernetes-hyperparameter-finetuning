503be33c8e10a35ca312bc3bfde9be98326b232b
SujanSharma07 <sujansharma202@gmail.com>
2024-10-26 13:06:50 +1100

    self hosted runner added

---
+++ best-model-deployment/self-hosted-runner/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+  
+ACCESS_TOKEN=$ACCESS_TOKEN
+REG_TOKEN=$REG_TOKEN
+REPO=$REPO
+OWNER=$OWNER
+HOSTNAME=$(hostname)
+
+
+# Repo Runner
+REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+
+
+echo $REG_TOKEN
+
+cd /home/runner/actions-runner
+
+./config.sh --url https://github.com/${OWNER}/${REPO} --token ${REG_TOKEN} --labels self-hosted,Linux,X64,${HOSTNAME}
+
+cleanup() {
+    echo "Removing runner..."
+    ./config.sh remove --unattended --token ${REG_TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
